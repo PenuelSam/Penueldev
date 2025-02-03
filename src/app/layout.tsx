a8538e8b7cffae7c6c4c5094686012839feb896e
PenuelSam <penuelsamuel54@gmail.com>
2025-02-03 01:43:54 +0100

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,10 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Penuel",
   description: "My Portfolio",
+  icons:{
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  }
 };
 
 export default function RootLayout({
